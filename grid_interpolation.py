6ecda666d468286753e8958e0cd72858acd41097
Zubchuk Eduard <ezubchuk@tempo.fit>
2022-08-08 19:16:10 +0700

    added documentation

--- grid_interpolation.py
+++ grid_interpolation.py
@@ -49,6 +49,7 @@ def get_grids_percent(e_grid_values, grid_values, rads):
         return 1
     return np.sum(e_grid_values[rads]) / np.sum(grid_values[rads])
 
+# Parser for arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--Case", help="Case folder name", type=str)
 parser.add_argument("-n", "--Normalize", help="Normalize grids (true/false)", type=bool, default=False)
@@ -58,6 +59,7 @@ parser.add_argument("-m", "--Min", help="Minimum number of plasts", type=int, de
 parser.add_argument("--Std", help="k coefficient for STD", type=float, default=0.2)
 args = parser.parse_args()
 
+# Reading config (custom thresholds)
 config_file = open('config.json')
 custom_target_percent = json.load(config_file)
 
@@ -71,9 +73,7 @@ d_plast_values = [] # Depth (thickness)
 plasts_n = len(fnmatch.filter(os.listdir(plast_dir), 'Porosity_STD_*'))
 print('Found %d plasts' % plasts_n)
 
-grid_x_size = 0
-grid_y_size = 0
-
+# Reading grid data
 for i in range(1, plasts_n + 1):
     print('Processing plast %d ...' % i)
 
@@ -133,15 +133,15 @@ b_percent = args.Threshold # percent threshold (in %)
 min_plasts = args.Min # minimum amount of plasts
 std_k = args.Std # coefficient for std in percent values
 
-x = np.arange(0, numX)
-y = np.arange(0, numY)
+x = np.arange(0, numX) # just range for X
+y = np.arange(0, numY) # and for Y
 
-optimization_dict = {}
+optimization_dict = {} # dictionary to store parameters
 
-robust_well_coords = (0, 0)
-max_robust_percentage = 0
-sum_percent = 0
-optimized_zone = []
+robust_well_coords = (0, 0) # saving proposed well coordinates
+max_robust_percentage = 0 # saving max percentage in interpolation radius
+sum_percent = 0 # saving max percentage sum in interpolation radius
+optimized_zone = [] # saving optimized zone mask
 
 p_flag = True
 all_ok = False
@@ -152,14 +152,14 @@ d_optimized_plast_values = d_plast_values.copy()
 
 b_failed_params = ['saturation', 'porosity', 'depth']
 
-for i in tqdm(range(0, len(s_grid_values))):
-    for j in range(0, len(s_grid_values[i])):
+for i in tqdm(range(0, len(s_grid_values))): # iterate through lines
+    for j in range(0, len(s_grid_values[i])): # and columns
         zero_count = 0
-        for c in range(len(s_plast_values)):
-            if s_plast_values[c][i][j] < 0.01 or p_plast_values[c][i][j] < 0.01 or d_plast_values[c][i][j] < 0.01:
+        for c in range(len(s_plast_values)): # check if plast isn't represented in this coordinate
+            if s_plast_values[c][i][j] < 0.1 or p_plast_values[c][i][j] < 0.1 or d_plast_values[c][i][j] < 0.1:
                 zero_count += 1
 
-        if zero_count > plasts_n - min_plasts:
+        if zero_count > plasts_n - min_plasts: # if number of zero plasts > required, skip
             continue
 
         a_optimized_plast_values = []
@@ -168,16 +168,18 @@ for i in tqdm(range(0, len(s_grid_values))):
 
         cx = float(j)
         cy = float(i)
-        mask = (y[np.newaxis, :] - cy) ** 2 + (x[:, np.newaxis] - cx) ** 2 < r ** 2
+        mask = (y[np.newaxis, :] - cy) ** 2 + (x[:, np.newaxis] - cx) ** 2 < r ** 2 # mask for interpolation radius
 
-        for c in range(len(s_plast_values)):
+        for c in range(len(s_plast_values)): # iterate through plasts
             s = s_plast_values.copy()
             p = p_plast_values.copy()
             d = d_plast_values.copy()
+
             c_s_grid_values = s[c].copy()
             c_p_grid_values = p[c].copy()
             c_d_grid_values = d[c].copy()
 
+            # checks if interpolation zone is not 0
             rad_flag = False
             for b in (c_s_grid_values[mask]):
                 if b < 0.1:
@@ -195,6 +197,7 @@ for i in tqdm(range(0, len(s_grid_values))):
             if rad_flag:
                 continue
 
+            # interpolation in zone (using atan)
             interp_rad = []
             for b in c_s_grid_values[mask]:
                 interp_rad.append(math.atan(b))
@@ -210,10 +213,12 @@ for i in tqdm(range(0, len(s_grid_values))):
                 interp_rad.append(math.atan(b))
             c_d_grid_values[mask] = interp_rad
 
+            # setting std at coordinate as 0
             c_s_grid_values[i][j] = 0.001
             c_p_grid_values[i][j] = 0.001
             c_d_grid_values[i][j] = 0.001
 
+            # count percents and save data to lists
             s_percent = (1 - get_grids_percent(c_s_grid_values, s[c], mask)) * 100
             p_percent = (1 - get_grids_percent(c_p_grid_values, p[c], mask)) * 100
             d_percent = (1 - get_grids_percent(c_d_grid_values, d[c], mask)) * 100
@@ -226,10 +231,12 @@ for i in tqdm(range(0, len(s_grid_values))):
 
         sum_p_plast = np.mean(plasts_percent_sums)
 
+        # properties threshold
         saturation_val = np.mean([x[0] for x in plasts_params_percent])
         porosity_val = np.mean([x[1] for x in plasts_params_percent])
         depth_val = np.mean([x[2] for x in plasts_params_percent])
 
+        # sum of all %s in plasts > threshold and > previous sum
         if (sum_p_plast >= b_percent and sum_percent < sum_p_plast):
             failed_params = []
             if custom_target_percent['saturation'] > saturation_val:
@@ -239,6 +246,7 @@ for i in tqdm(range(0, len(s_grid_values))):
             if custom_target_percent['depth'] > depth_val:
                 failed_params.append('depth')
 
+            # check if it doesn't pass threshold for any property
             if len(failed_params) > 0:
                 if len(b_failed_params) >= len(failed_params):
                     b_failed_params = failed_params
@@ -246,6 +254,7 @@ for i in tqdm(range(0, len(s_grid_values))):
                 if len(b_failed_params) >= len(failed_params):
                     b_failed_params = failed_params
 
+                # save the data for output
                 for c in range(len(a_optimized_plast_values)):
                     s_optimized_plast_values[c] = a_optimized_plast_values[c][0].copy()
                     p_optimized_plast_values[c] = a_optimized_plast_values[c][1].copy()
@@ -257,9 +266,9 @@ for i in tqdm(range(0, len(s_grid_values))):
                 all_ok = True
                 optimized_zone = mask
                 sum_percent = sum_p
-        elif all_ok:
+        elif all_ok: # in case we already found zone with all 3 properties suitable
             continue
-        elif sum_p_plast >= b_percent:
+        elif sum_p_plast >= b_percent: # if we didn't do that, but passed the threshold
             temp_grids = [s_plast_values, s_plast_values, s_plast_values]
 
             if max([x[0] for x in plasts_params_percent]) >= b_percent:
@@ -284,23 +293,25 @@ for i in tqdm(range(0, len(s_grid_values))):
             robust_well_coords = (i, j)
             max_robust_percentage = max(max_robust_percentage, sum_p_plast)
 
+        # saving data to dict
         optimization_dict[(i, j)] = {'saturation': saturation_val,
                                      'porosity': porosity_val,
                                      'depth': depth_val,
                                      'coeff': sum_p_plast}
 
-if p_flag:
+if p_flag: # didn't find suitable zone for threshold
     print('Threshold percentage set:', b_percent)
     print('Largest decrease in percent:', max_robust_percentage)
     print('Please, reduce the threshold')
 
-if len(b_failed_params) > 0:
+if len(b_failed_params) > 0: # didn't find suitable zone for properties threshold
     print('Optimization did not pass the threshold for these parameters:')
     for p in b_failed_params:
         print(p)
 else:
     print('Optimization complete. Proposed well coordinates is', robust_well_coords)
 
+# print the statistic
 max_saturation = 0
 max_porosity = 0
 max_depth = 0
@@ -320,6 +331,7 @@ print('Saturation: %d' % int(max_saturation))
 print('Porosity: %d' % int(max_porosity))
 print('Depth: %d' % int(max_depth))
 
+# saving data in output files
 for i in range(1, plasts_n+1):
     files = ['Saturation_zone_%d'%i, 'Porosity_zone_%d'%i, 'Depth_zone_%d'%i]
     j = 0
